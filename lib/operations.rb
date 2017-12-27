d1c481d9e1fa56da3b76cc81bcb715b351adc9ea
Shivani Mathur <qwellery@gmail.com>
2017-12-27 11:36:25 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,17 @@
 def unsafe?(speed)
-	if speed > 60 || speed < 40
-		return true
-	else
+	if speed.between?(40,60)
 		return false
+	else
+		return true
 	end
 end
 
 
 
 def not_safe?(speed)
-
+	if speed < 60 || speed > 40
+		return true
+	else
+		return false
+	end
 end
