3acecc850fb3b0ce26960441d7b83d847314b902
Shivani Mathur <qwellery@gmail.com>
2017-12-27 11:39:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+	if speed.between?(40,60)
+		return false
+	else
+		return true
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
